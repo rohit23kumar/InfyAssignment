44b7ce3df82c6f43a5cdc42d421410c78ef3c75a
rohit kumar <rohitkumar1923@gmail.com>
2018-07-31 00:26:09 +0530

    Constants Files Being added

---
+++ InfyAssignment/Color+Constants.swift
@@ -0,0 +1,19 @@
+//
+//  Color+Constants.swift
+//  InfyAssignment
+//
+//  Created by Rohit Kumar on 30/07/18.
+//  Copyright © 2018 Rohit Kumar. All rights reserved.
+//
+
+import Foundation
+import UIKit
+struct ColorConstants {
+    //  COLOR CONSTANT
+    
+    static let kColor_Navigation_Bar_Color = UIColor(red: 87.0/255.0, green: 159.0/255.0, blue: 43.0/255.0, alpha: 1.0)
+    static let kColor_Even_Cell_Color = UIColor(red: 255.0/255.0, green: 212.0/255.0, blue: 121.0/255.0, alpha: 1.0)
+    static let kColor_Odd_Cell_Color = UIColor(red: 214.0/255.0, green: 214.0/255.0, blue: 214.0/255.0, alpha: 1.0)
+    static let kColor_Text_Color = UIColor(red: 4.0/255.0, green: 51.0/255.0, blue: 255.0/255.0, alpha: 1.0)
+    
+}
---
+++ InfyAssignment/String+Constants.swift
@@ -0,0 +1,22 @@
+//
+//  Constants.swift
+//  InfyAssignment
+//
+//  Created by Rohit Kumar on 30/07/18.
+//  Copyright © 2018 Rohit Kumar. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    static let k_jsonURL = "https://dl.dropboxusercontent.com/s/2iodh4vg0eortkl/facts.json"
+    static let k_jsonSerialisationErrorMsg = "Error occured during json serialisation: "
+    static let k_NOT_CONNECTED = "Not Connected"
+    static let k_NOT_CONNECTED_MSG = "Phone is not connected to internet."
+    static let k_DISMISS = "Dismiss"
+    static let k_CELLIDENTIFIER = "cellIdentifier"
+    static let k_DefaultTitle = "Title Label Loading...."
+    static let k_DefaultDescription = "Description Label Loading...."
+    static let k_DefaultFont = "Helvetica Neue"
+    
+}
